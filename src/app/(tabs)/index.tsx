3c82201621dbf4aaac3f689293bc080d2a99fabe
Diego <diegotimao1104@gmail.com>
2024-04-05 19:27:33 -0300

    feat: Add fonts, colors and component loading in file _layout.tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import Header from '@/components/Header'
+import React from 'react'
+import { View } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-slate-400">
-      <Text>Home Greencycle</Text>
-      <Text>Fazer a tela principal do sistema</Text>
+    <View className="flex-1 bg-gray-700">
+      <Header/>
     </View>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { AlignLeft } from "lucide-react-native";
+import { View, Text } from "react-native";
+
+export default function Header() {
+  return (
+    <View>
+      <View>
+        <AlignLeft color={"#000000"}/>
+      </View>
+    </View>
+  )
+}
