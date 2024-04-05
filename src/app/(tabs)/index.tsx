0c0ad02dcb0af6a709f2037ce427056d1a098040
Diego <diegotimao1104@gmail.com>
2024-04-04 21:08:55 -0300

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-slate-400">
+      <Text>Home Greencycle</Text>
+      <Text>Fazer a tela principal do sistema</Text>
+    </View>
+  )
+}
