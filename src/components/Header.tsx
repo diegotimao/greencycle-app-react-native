2dff19696f1c905252e8c67e465d476e9dfde2ab
Diego <diegotimao1104@gmail.com>
2024-04-10 02:22:01 -0300

    feat: Adjusting Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
-import { AlignLeft, BellDot } from "lucide-react-native";
+import { AlignLeft, BellDot, ChevronRight } from "lucide-react-native";
 import { View, Text } from "react-native";
 
 export default function Header() {
   return (
-    <View className="mt-7 p-5 h-60 w-full bg-green-900 justify-between">
+    <View className="mt-6 p-5 h-60 w-full bg-green-900 justify-between">
       <View className="flex-row justify-between">
-        <AlignLeft color={"#ffffff"} />
+        <AlignLeft color={"#ffffff"} size={35}/>
         <View className="h-10 w-10 items-center justify-center rounded-full bg-green-700">
           <BellDot color={"#ffffff"} />
         </View>
-      </View>
+      </View>''
 
       <View className="flex-row justify-between mb-12">
         <View className="gap-2">
@@ -17,10 +17,11 @@ export default function Header() {
           <Text className="text-white">Total reciclado</Text>
         </View>
 
-        <View className="gap-2 items-end">
+        <View className="gap-2 items-end justify-end">
           <Text className="font-medium text-2xl text-white">R$ 21,60</Text>
-          <View>
+          <View className="flex-row items-center justify-end">
             <Text className="text-white">Saldo disponível</Text>
+            <ChevronRight color={`#ffffff`} />
           </View>
         </View>
       </View>
