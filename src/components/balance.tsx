40c8fdf4fb6440d87a6de4178b638a689add0c9e
Diego <diegotimao1104@gmail.com>
2024-04-10 02:55:59 -0300

    feat: Creating structure Balance

---
+++ src/components/balance.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+export default function Balance() {
+  return (
+    <View className="w-full h-24 p-5">
+      <View className="bg-white w-full h-80 mt-[-50] rounded-xl justify-center">
+        
+      </View>
+    </View>
+  )
+}
