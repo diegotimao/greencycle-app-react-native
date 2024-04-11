5dce8c62f8aff5f43f6800b8ef34ae33bbbc435c
Diego <diegotimao1104@gmail.com>
2024-04-10 21:06:13 -0300

    feat: Add estructure and layout component balance

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -1,11 +1,21 @@
-import { View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import ItemListBalance from "./item-list-balance";
+import { itemsBalance } from "@/utils/items-balance";
 
 export default function Balance() {
   return (
     <View className="w-full h-24 p-5">
-      <View className="bg-white w-full h-80 mt-[-50] rounded-xl justify-center">
-        
-      </View>
+      <View className="bg-white w-full h-['300px'] mt-[-50] rounded-xl p-5">
+        <View className="gap-2 mb-6">
+          <Text className="text-blue-900 font-bold text-xl">Meu balanço de Outubro</Text>
+          <Text className="text-blue-900 font-semibold text-3xl ">R$ 21,60</Text>
+        </View>
+        <FlatList 
+         data={itemsBalance}
+         keyExtractor={(item) => item.id}
+         renderItem={({ item }) => <ItemListBalance data={item} />}
+        />
+      </View> 
     </View>
   )
 }
