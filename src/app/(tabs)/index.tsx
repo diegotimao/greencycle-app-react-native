2b55dcc15cbed81aaaaa70d911741b0d9831db6a
Diego <diegotimao1104@gmail.com>
2024-04-11 12:02:59 -0300

    fix: Correct import component in file index.tsx

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,17 @@
-import Header from '@/components/header'
-import Balance from '@/components/balance'
 import React from 'react'
 import { View } from 'react-native'
+
+import Header from '@/components/header'
+import Balance from '@/components/balance'
+import SectionButtonsPage from '@/components/section-buttons-page'
+
 
 export default function Home() {
   return (
     <View className="flex-1 bg-gray-700">
       <Header/>
       <Balance />
+      <SectionButtonsPage />
     </View>
   )
 }
