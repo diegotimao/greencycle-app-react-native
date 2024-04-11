28eb4e999962a81f9f8f93f0148b4f346093d384
Diego <diegotimao1104@gmail.com>
2024-04-10 21:07:23 -0300

    feat: Create object items balance

---
+++ src/utils/items-balance.ts
@@ -0,0 +1,9 @@
+export type itemsBalanceDataProps = (typeof itemsBalance)[0]
+
+export const itemsBalance = [
+  { id: '1', nameItem: 'Plástico', kg: 3.8, value: 10.50 },
+  { id: '2', nameItem: 'Papel', kg: 2.5, value: 7.80 },
+  { id: '3', nameItem: 'Alumínio', kg: 1.2, value: 15.20 },
+  { id: '4', nameItem: 'Vidro', kg: 4.10, value: 8.90 },
+  { id: '5', nameItem: 'Ferro', kg: 6.7, value: 20.40 }
+]
