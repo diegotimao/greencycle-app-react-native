0a38ce214d6336f1738ca57d0a60e52e305e4f9d
Diego <diegotimao1104@gmail.com>
2024-04-12 06:14:17 -0300

    feat: Create router locations

---
+++ src/app/(tabs)/locations.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Locations() {
+  return (
+    <View className="flex-1 bg-gray-700">
+      <Text>Locations</Text>
+    </View>
+  )
+}
