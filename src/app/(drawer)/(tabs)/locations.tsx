f88b56fcec4110022c4b626e1e1ce9047d76a81b
Diego <diegotimao1104@gmail.com>
2024-04-17 10:50:09 -0300

    feat: Create new screen locations

--- src/app/(drawer)/(tabs)/locations.tsx
+++ src/app/(drawer)/(tabs)/locations.tsx
@@ -1,9 +1,10 @@
+import HeaderAlter from "@/components/header-alter";
 import { Text, View } from "react-native";
 
 export default function Locations() {
   return (
     <View className="flex-1 bg-gray-700">
-      <Text>Locations</Text>
+      <HeaderAlter />
     </View>
   )
 }
