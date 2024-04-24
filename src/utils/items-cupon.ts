2d081108c73d730aa6fb5d50fc19b708da0bac36
Diego <diegotimao1104@gmail.com>
2024-04-24 10:49:47 -0300

    feat: Creating mock DataArray items-cupon

---
+++ src/utils/items-cupon.ts
@@ -0,0 +1,40 @@
+export type itemCupon = (typeof itemCupon)[0]
+
+export const itemCupon = [
+  {
+    imageLogo: require('@/assets/logo-mac.png'),
+    valueDiscont: 10,
+    company: "McDonalds",
+    validade: "20-04-2024"
+  },
+  {
+    imageLogo: require('@/assets/logo-ifood.png'),
+    valueDiscont: 10,
+    company: "ifood",
+    validade: "10-04-2024"
+  },
+  {
+    imageLogo: require('@/assets/logo-mercado.png'),
+    valueDiscont: 80,
+    company: "Mercado Livre",
+    validade: "05-04-2024"
+  },
+  {
+    imageLogo: require('@/assets/logo-amazon.png'),
+    valueDiscont: 25,
+    company: "Amazon",
+    validade: "24-04-2024"
+  },
+  {
+    imageLogo: require('@/assets/logo-uber.png'),
+    valueDiscont: 35,
+    company: "Uber",
+    validade: "10-05-2024"
+  },
+  {
+    imageLogo: require('@/assets/logo-bobs.png'),
+    valueDiscont: 15,
+    company: "Bobs",
+    validade: "18-04-2024"
+  }
+]
