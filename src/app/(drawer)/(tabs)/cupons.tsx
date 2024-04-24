af834f4c92ad15f9d5ac0ce319fcd0607acc3638
Diego <diegotimao1104@gmail.com>
2024-04-24 10:48:03 -0300

    feat: Creating screen cupons

--- src/app/(drawer)/(tabs)/cupons.tsx
+++ src/app/(drawer)/(tabs)/cupons.tsx
@@ -1,9 +1,16 @@
-import { Text, View } from "react-native";
+import CuponItem from "@/components/cupon-item";
+import CuponsList from "@/components/cupons-list";
+import Header from "@/components/header";
+import { ScrollView, Text, View, Image, Pressable } from "react-native";
 
 export default function Cupons() {
   return (
-    <View>
-      <Text>Cupons</Text>
+    <View className="flex-1 bg-gray-700">
+      <Header />
+      <View className="p-5">
+        <Text className="text-lg font-semibold text-blue-800">Cupons disponíveis</Text>
+      </View>
+      <CuponsList/>
     </View>
   )
 }
