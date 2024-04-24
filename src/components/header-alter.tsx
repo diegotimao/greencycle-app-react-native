44513362d54e5b6b19ba552bee17c90e4852a4db
Diego <diegotimao1104@gmail.com>
2024-04-24 10:50:13 -0300

    feat: Add new header-alternative

--- src/components/header-alter.tsx
+++ src/components/header-alter.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { ChevronLeft } from 'lucide-react-native';
+import { ChevronLeft, Filter } from 'lucide-react-native';
 import { Pressable, View, Text } from 'react-native';
 
 export default function HeaderAlter() {
   return (
-    <View className="mt-6 pl-10 pr-10 pt-5 pb-5 flex-row justify-between items-center">
-      <Pressable className="w-12 h-12 bg-white rounded-full items-center justify-center">
-        <ChevronLeft color="#18604A" size={32} />
+    <View className="mt-6 pl-6 pr-6 pt-5 pb-5 flex-row justify-between items-center">
+      <Pressable className="w-12 h-12 justify-center">
+        <ChevronLeft color={"#18604A"} size={24} />
       </Pressable>
-      <Text className="flex-1 text-xl text-center text-green-900 font-normal">Pontos de coleta</Text>
+      <Text className="text-xl text-green-900 font-bold">Pontos de coleta</Text>
+      <Pressable className="w-12 h-12 justify-center">
+        <Filter color={"#18604A"} size={24} />
+      </Pressable>
     </View>
   );
 }
