bee053173364ef5f27370af54394452f25abd270
Diego <diegotimao1104@gmail.com>
2024-04-24 20:04:32 -0300

    feat: Add props component HeaderAlter

--- src/components/header-alter.tsx
+++ src/components/header-alter.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { ChevronLeft, Filter } from 'lucide-react-native';
 import { Pressable, View, Text } from 'react-native';
 
-export default function HeaderAlter() {
+type headerAltProps = {
+  pageTitle: string,
+  isFilter: boolean
+}
+
+export default function HeaderAlter({ pageTitle, isFilter }: headerAltProps) {
   return (
     <View className="mt-6 pl-6 pr-6 pt-5 pb-5 flex-row justify-between items-center">
       <Pressable className="w-12 h-12 justify-center">
         <ChevronLeft color={"#18604A"} size={24} />
       </Pressable>
-      <Text className="text-xl text-green-900 font-bold">Pontos de coleta</Text>
-      <Pressable className="w-12 h-12 justify-center">
+      <Text className={`text-xl text-green-900 font-bold ${!isFilter ? 'flex-1 text-center' : null}`}>{pageTitle}</Text>
+      {isFilter ? (
+        <Pressable className="w-12 h-12 justify-center">
         <Filter color={"#18604A"} size={24} />
       </Pressable>
+      ) : null}
     </View>
   );
 }
