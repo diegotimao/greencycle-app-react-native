b584c3f5a7a0d8c30c3335837fd6742250d93505
Diego <diegotimao1104@gmail.com>
2024-04-29 21:30:15 -0300

    fix: Correct button next

--- src/components/header-alter.tsx
+++ src/components/header-alter.tsx
@@ -10,7 +10,7 @@ type headerAltProps = {
 export default function HeaderAlter({ pageTitle, isFilter }: headerAltProps) {
   return (
     <View className="mt-6 pl-6 pr-6 pt-5 pb-5 flex-row justify-between items-center">
-      <Pressable className="w-12 h-12 justify-center">
+      <Pressable className="justify-center">
         <ChevronLeft color={"#18604A"} size={24} />
       </Pressable>
       <Text className={`text-xl text-green-900 font-bold ${!isFilter ? 'flex-1 text-center' : null}`}>{pageTitle}</Text>
