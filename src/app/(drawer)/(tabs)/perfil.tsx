c952521976aa08e8e8ccd5f60e0efafb7d2c555f
Diego <diegotimao1104@gmail.com>
2024-04-30 19:06:55 -0300

    refactor: Refatoring screen Profile

--- src/app/(drawer)/(tabs)/perfil.tsx
+++ src/app/(drawer)/(tabs)/perfil.tsx
@@ -1,12 +1,15 @@
-import HeaderAlter from "@/components/header-alter";
-import { useRouter } from "expo-router";
-import { BadgeHelp, ChevronRight, CircleUserRound, LogOut, Repeat1, User2, WalletMinimal } from "lucide-react-native";
 import React from "react";
-import { View, Image, Text, Pressable } from "react-native";
+import { View } from "react-native";
+import { useRouter } from "expo-router";
+import { BadgeHelp, CircleUserRound, LogOut, Repeat1, WalletMinimal } from "lucide-react-native";
+
+import HeaderAlter from "@/components/header-alter";
+import ProfilePressable from "@/components/profile-pressable";
+import ProfileHeader from "@/components/profile-header";
 
 export default function Perfil() {
   const routes = useRouter();
-
+  
   const logout = () => {
     routes.push("/login")
   }
@@ -16,53 +19,13 @@ export default function Perfil() {
       <HeaderAlter isFilter={false} pageTitle="Perfil" />
       <View className="w-full h-50 p-6">
         <View className="mt-[-50] w-full gap-10 rounded-xl">
-          <View className="w-full flex-row items-center p-2 gap-2 bg-white/60 border border-gray-400/20 rounded-md">
-            <Image source={require('@/assets/logo-ferrovelho.png')} className="rounded-full" />
-            <View className="gap-1">
-              <Text className="text-sm font-bold">Daniel Ferro Velho</Text>
-              <Text className="text-sm">danielferrovelho@gmail.com</Text>
-            </View>
-          </View>
-
+          <ProfileHeader />
           <View className="w-full gap-3">
-            <Pressable className="w-full h-14 bg-white rounded-md flex-row justify-between items-center pl-4 pr-4">
-              <View className="flex-row items-center gap-4">
-                <CircleUserRound color={"#18604A"} />
-                <Text>Editar Perfil</Text>
-              </View>
-              <ChevronRight color={"#18604A"} />
-            </Pressable>
-            <Pressable className="w-full h-14 bg-white rounded-md flex-row justify-between items-center pl-4 pr-4">
-              <View className="flex-row items-center gap-4">
-                <WalletMinimal color={"#18604A"} />
-                <Text>Métodos de pagamentos</Text>
-              </View>
-              <ChevronRight color={"#18604A"} />
-            </Pressable>
-            <Pressable className="w-full h-14 bg-white rounded-md flex-row justify-between items-center pl-4 pr-4">
-              <View className="flex-row items-center gap-4">
-                <BadgeHelp color={"#18604A"} />
-                <Text>Suporte</Text>
-              </View>
-              <ChevronRight color={"#18604A"} />
-            </Pressable>
-            <Pressable className="w-full h-14 bg-white rounded-md flex-row justify-between items-center pl-4 pr-4">
-              <View className="flex-row items-center gap-4">
-                <Repeat1 color={"#18604A"} />
-                <Text>Faq</Text>
-              </View>
-              <ChevronRight color={"#18604A"} />
-            </Pressable>
-            <Pressable
-              onPress={() => logout()}
-              className="w-full h-14 bg-white rounded-md flex-row justify-between items-center pl-4 pr-4"
-            >
-              <View className="flex-row items-center gap-4">
-                <LogOut color={"#18604A"} />
-                <Text>Sair do app</Text>
-              </View>
-              <ChevronRight color={"#18604A"} />
-            </Pressable>
+            <ProfilePressable text="Editar Perfil" icon={CircleUserRound} iconColor="#18604A" />
+            <ProfilePressable text="Métodos de pagamentos" icon={WalletMinimal} iconColor="#18604A" />
+            <ProfilePressable text="Suporte" icon={BadgeHelp} iconColor="#18604A" />
+            <ProfilePressable text="Faq" icon={Repeat1} iconColor="#18604A" />
+            <ProfilePressable text="Sair do app" icon={LogOut} iconColor="#18604A" />
           </View>
         </View>
       </View>
