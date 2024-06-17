8f69e5bde38eb6d270f58b9185c6d9e8537039bd
Diego <diegotimao1104@gmail.com>
2024-06-17 13:17:45 -0300

    feat: Create component screen register-user-data

--- src/app/register-user-adress.tsx
+++ src/app/register-user-adress.tsx
@@ -1,21 +1,54 @@
+import HeaderRegister from '@/components/header-register';
 import Select from '@/components/select';
-import { ChevronDown, TextSelect } from 'lucide-react-native';
-import React, { useState } from 'react';
+import { AuthContext } from '@/contexts/auth';
+import { useRouter } from 'expo-router';
+import React, { useContext, useEffect, useState } from 'react';
 import { KeyboardAvoidingView, Pressable, ScrollView, Text, View, Platform, Image, Touchable, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
 
 const { width } = Dimensions.get('window');
 
 export default function RegisterUserData() {
-  // const [focusedInput, setFocusedInput] = useState(null);
+  const route = useRouter();
+  const authContext = useContext(AuthContext);
 
-  // const handleFocus = (input) => {
-  //   setFocusedInput(input);
-  // };
+  const [disableSelectCity, setDisableCity] = useState(true);
+  const [stateIdSected, setStateIdSected] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
 
-  // const handleBlur = () => {
-  //   setFocusedInput(null);
-  // };
-  const [stateId, setStateId] = useState('');
+  if (!authContext) {
+    throw new Error('AuthContext not found')
+  };
+
+  const { stateId, setStateId } = authContext;
+  
+  useEffect(() => {
+    if (stateId !== '') {
+      setDisableCity(false);
+    } else {
+      setDisableCity(true);
+    }
+  });
+
+  const handleStateId = (id: string, name: string) => {
+    setState(name);
+    setDisableCity(false);
+    setStateId(id);
+    return setStateIdSected(id);
+  }
+
+  const handleSelectCity = (id: string, city: string) => {
+    setCity(city);
+  }
+
+  const handleClickBackPage = () => {
+    setStateId('');
+    return route.back();
+  };
+  
+  const handleClickSubmit = () => {
+    console.log(state, city)
+  }
 
   return (
     <KeyboardAvoidingView
@@ -27,25 +60,27 @@ export default function RegisterUserData() {
         contentContainerStyle={{ flexGrow: 1 }}
       >
         <View style={{ flex: 1, marginTop: 24, padding: 24, paddingTop: 8, justifyContent: 'space-between' }}>
-          <View style={{ flexDirection: 'row', marginTop: 16, height: 128, justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ alignItems: 'center' }}>
-              <Image source={require('@/assets/icon-greencycle.png')} />
-              <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Vamos começar</Text>
-              <Text style={{ fontSize: 16, marginTop: 4 }}>Entre com os dados para concluir o cadastro</Text>
-            </View>
-          </View>
-
+          <HeaderRegister />
           <View className='flex-1 justify-and mt-10'>
             <View className="">
               <View className='flex-col gap-4 mt-8'>
-                <Select title='Selecione o estado' search='state' stateId={stateId} onChangeSelect={(id) => setStateId(id)}/>
-                <Select title='Selecione a cidade' search='city' stateId={stateId} onChangeSelect={(id) => setStateId(id)}/>
+                <Select title='Selecione o estado' search='state' stateId={stateId} onChangeSelect={(id, name) => handleStateId(id, name)} />
+                {!disableSelectCity ? <Select title='Selecione a cidade' search='city' stateId={stateIdSected} onChangeSelect={(id, name) => handleSelectCity(id, name)} /> : null}
               </View>
             </View>
           </View>
-          <TouchableOpacity className='mt-5 h-16 w-full bg-green-900 rounded-full items-center justify-center'>
-            <Text className="text-white text-md font-bold">Finalizar cadastro</Text>
-          </TouchableOpacity>
+          <View className='flex-col gap-3'>
+            <TouchableOpacity className='h-16 w-full bg-white border border-green-900 rounded-full items-center justify-center' onPress={handleClickBackPage}>
+              <Text className="text-green-900  text-md font-bold">Voltar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              className='h-16 w-full bg-green-900 rounded-full items-center justify-center'
+              disabled={false}
+              onPress={handleClickSubmit}
+            >
+              <Text className="text-white text-md font-bold">Finalizar cadastro</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
