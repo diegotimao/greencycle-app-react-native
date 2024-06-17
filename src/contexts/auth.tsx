df58559c20543bfa4d0ee52c3ccbece2f240018b
Diego <diegotimao1104@gmail.com>
2024-06-17 13:15:19 -0300

    feat: Create auth user contetext

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,17 +1,58 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, ReactNode } from "react";
 
+// Defina a interface para o contexto
 interface AuthContextType {
-  name: String
+  name: string,
+  stateId: string;
+  setStateId: (id: string) => void;
+  user: {
+    name: string,
+    cpf: string,
+    email: string,
+    password: string,
+    state: string,
+    city: string
+  };
+  setUser: (user: {
+    name: string,
+    cpf: string,
+    email: string,
+    password: string,
+    state: string,
+    city: string
+  }) => void
 }
 
-export const AuthContext = createContext<AuthContextType>({ name: ""});
+// Crie o contexto com um valor padrão
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-function AuthProvider({ children }: any) {
+// Crie o AuthProvider
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [stateId, setStateId] = useState<string>("");
+  const [user, setUser] = useState({
+    name: '',
+    cpf: '',
+    email: '',
+    password: '',
+    state: '',
+    city: ''
+  });
+
   return (
-    <AuthContext.Provider value={{ name: "" }}>
+    <AuthContext.Provider value={{
+      name: user.name,
+      stateId, 
+      setStateId,
+      user,
+      setUser
+    }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export default AuthProvider;
