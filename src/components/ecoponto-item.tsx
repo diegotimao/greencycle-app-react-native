d49a1dcf0b39fd8f4b33d93b478cbf1c45e8638d
Diego <diegotimao1104@gmail.com>
2024-06-26 00:56:20 -0300

    feat: Alter style in component EcopontoItem

--- src/components/ecoponto-item.tsx
+++ src/components/ecoponto-item.tsx
@@ -18,14 +18,14 @@ export default function EcopontoItem({ ecopontoItem }: ecopontoProps) {
   return (
     <Pressable 
       onPress={() => navigation()}
-      className="w-full flex-row gap-4 p-6 justify-between items-center bg-white rounded-xl mb-3">
+      className="w-full flex-row gap-4 p-5 justify-between items-center bg-white rounded-xl mb-3">
       <View className="w-10 h-10 items-center justify-center">
         <Image source={ecopontoItem.logoUrl} className="rounded-full" />
       </View>
       <View className="-ml-12">
         <Text className="text-md font-bold">{ecopontoItem.name}</Text>
-        <Text className="text-sm text-gray-600">Rua: {ecopontoItem.address.road} N: {ecopontoItem.address.number}</Text>
-        <Text className="text-sm text-gray-600">{ecopontoItem.address.city} {ecopontoItem.address.country}</Text>
+        <Text className="text-sm text-zinc-700 mt-2">Rua: {ecopontoItem.address.road} N: {ecopontoItem.address.number}</Text>
+        <Text className="text-sm text-zinc-700 mt-1">{ecopontoItem.address.city} {ecopontoItem.address.country}</Text>
       </View>
       <ChevronRight color="#8E8E93" size={24} />
     </Pressable>
