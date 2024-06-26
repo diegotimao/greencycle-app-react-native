e2a7ac3c3ddca7a038007190b5546fb501477dfa
Diego <diegotimao1104@gmail.com>
2024-06-26 05:22:32 -0300

    feat: Alter style border component ecoponto-item

--- src/components/ecoponto-item.tsx
+++ src/components/ecoponto-item.tsx
@@ -18,16 +18,16 @@ export default function EcopontoItem({ ecopontoItem }: ecopontoProps) {
   return (
     <Pressable 
       onPress={() => navigation()}
-      className="w-full flex-row gap-4 p-5 justify-between items-center bg-white rounded-xl mb-3">
+      className="w-full flex-row gap-4 p-5 justify-between items-center bg-white border border-zinc-500/5 rounded-xl mb-3">
       <View className="w-10 h-10 items-center justify-center">
         <Image source={ecopontoItem.logoUrl} className="rounded-full" />
       </View>
       <View className="-ml-12">
-        <Text className="text-md font-bold">{ecopontoItem.name}</Text>
-        <Text className="text-sm text-zinc-700 mt-2">Rua: {ecopontoItem.address.road} N: {ecopontoItem.address.number}</Text>
-        <Text className="text-sm text-zinc-700 mt-1">{ecopontoItem.address.city} {ecopontoItem.address.country}</Text>
+        <Text className="text-md font-bold text-black-900/70">{ecopontoItem.name}</Text>
+        <Text className="text-sm text-zinc-700/90 mt-2">Rua: {ecopontoItem.address.road} N: {ecopontoItem.address.number}</Text>
+        <Text className="text-sm text-zinc-700/90 mt-1">{ecopontoItem.address.city} {ecopontoItem.address.country}</Text>
       </View>
-      <ChevronRight color="#8E8E93" size={24} />
+      <ChevronRight color="#064E3B80" size={24} />
     </Pressable>
   )
 }
