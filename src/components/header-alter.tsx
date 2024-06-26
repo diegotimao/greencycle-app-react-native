ad826d8b11b039807f8932bcd19d7623445e1284
Diego <diegotimao1104@gmail.com>
2024-06-26 00:55:32 -0300

    feat: Implement button back in component header-alter

--- src/components/header-alter.tsx
+++ src/components/header-alter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ChevronLeft, Filter } from 'lucide-react-native';
 import { Pressable, View, Text } from 'react-native';
+import { router } from 'expo-router';
 
 type headerAltProps = {
   pageTitle: string,
@@ -10,7 +11,7 @@ type headerAltProps = {
 export default function HeaderAlter({ pageTitle, isFilter }: headerAltProps) {
   return (
     <View className="mt-6 pl-6 pr-6 pt-5 pb-5 flex-row justify-between items-center">
-      <Pressable className="justify-center">
+      <Pressable className="justify-center" onPress={() => router.back()}>
         <ChevronLeft color={"#18604A"} size={24} />
       </Pressable>
       <Text className={`text-xl text-green-900 font-bold ${!isFilter ? 'flex-1 text-center' : null}`}>{pageTitle}</Text>
