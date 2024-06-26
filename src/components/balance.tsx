58973880a341f28e4e4fa0bfd8cd5298cb4cfe54
Diego <diegotimao1104@gmail.com>
2024-06-26 05:21:05 -0300

    feat: Alter style border component Balance

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -5,7 +5,7 @@ import { itemsBalance } from "@/utils/items-balance";
 export default function Balance() {
   return (
     <View className="w-full p-5">
-      <View className="bg-white w-full mt-[-50] rounded-xl p-5">
+      <View className="bg-white border border-zinc-500/5 w-full mt-[-50] rounded-xl p-5">
         <View className="gap-2 mb-4">
           <Text className="text-blue-900 font-bold text-xl">Meu balanço de Outubro</Text>
           <Text className="text-blue-900 font-semibold text-2xl">R$ 21,60</Text>
