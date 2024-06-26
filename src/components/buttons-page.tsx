f45ebd3db92431832b04ddf884e2427283384380
Diego <diegotimao1104@gmail.com>
2024-06-26 05:21:41 -0300

    feat: Alter style border and color component ButtonsPage

--- src/components/buttons-page.tsx
+++ src/components/buttons-page.tsx
@@ -7,11 +7,11 @@ type ButtonsProps = {
 
 export default function ButtonsPage({ title }: ButtonsProps) {
   return (
-    <Pressable className="w-[48%] bg-white h-32 rounded-lg p-5 gap-4">
-      <View className="w-12 h-12 rounded-full bg-green-900 justify-center items-center">
+    <Pressable className="w-[48%] bg-white border border-zinc-500/5 h-32 rounded-lg p-5 gap-4">
+      <View className="w-12 h-12 rounded-full bg-green-900/80 justify-center items-center">
         {title === 'Estatística Geral' ? <BarChart3 color={'white'} size={20}/> : <GraduationCap color={'white'} />}
       </View>
-      <Text className="font-bold text-green-900">{title}</Text>
+      <Text className="font-bold text-green-900/80">{title}</Text>
     </Pressable>
   );
 };
