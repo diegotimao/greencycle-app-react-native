5b9fd4b5a30c9c9ad51d99d2544c69db3674ab3a
Diego <diegotimao1104@gmail.com>
2024-06-26 02:14:28 -0300

    feat: Alter style image

--- src/components/profile-header.tsx
+++ src/components/profile-header.tsx
@@ -4,13 +4,13 @@ import { useContext } from "react";
 import { AuthContext } from "@/contexts/auth";
 
 export default function ProfileHeader() {
-  const { name } = useContext(AuthContext);
+  // const { name } = useContext(AuthContext);
 
   return (
     <View className="w-full items-center p-2 gap-2">
-      <Image source={require('@/assets/avatar-user.jpg')} className="rounded-full w-24 h-24 border border-gray-500/60 border-spacing-1" />
+      <Image source={require('@/assets/avatar-user.jpg')} className="rounded-2xl w-24 h-24 border " />
       <View className="gap-1 items-center">
-        <Text className="text-2xl font-bold">{name}</Text>
+        <Text className="text-2xl font-bold">Diego dos Santos</Text>
         <Text className="text-sm font-medium">diegotimao1104@gmail.com</Text>
       </View>
     </View>
