b828c3909de10d9b26e540eb2ca04085e2b3adc9
Diego <diegotimao1104@gmail.com>
2024-06-29 11:47:25 -0300

    feat: Create logic setUser in AppLayout

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,10 +1,30 @@
 import { Redirect, Stack } from 'expo-router';
-
 import { useSession } from '@/contexts/authSession';
 import { Text } from 'react-native';
+import { useStorageState } from '@/utils/useStorageState';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '@/contexts/auth';
+import { User } from '@/interfaces/User.interface';
+import { jwtDecode } from 'jwt-decode';
 
 export default function AppLayout() {
   const { session, isLoading } = useSession();
+  const [state, setValue] = useStorageState('session');
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    throw new Error('AuthContext não foi fornecido corretamente.');
+  }
+
+  const { setUser, setToken } = authContext;
+
+  useEffect(() => {
+    if (typeof state[1] === 'string') {
+      const tokenDecoded = jwtDecode(state[1]);
+      setUser(tokenDecoded as User);
+      setToken(state[1]);
+    }
+  }, [state, setUser]);
 
   if (isLoading) {
     return <Text>Loading...</Text>;
@@ -14,5 +34,5 @@ export default function AppLayout() {
     return <Redirect href="/sign-in" />;
   }
 
-  return <Stack screenOptions={{ headerShown: false }}/>;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
